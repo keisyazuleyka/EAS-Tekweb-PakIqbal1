476b3c728f03df51b1198dc239f7cb2fa5733fb0
Nasya Khaila Alifianka <nasyakhaila142@gmail.com>
2026-07-06 08:20:29 +0700

    Force HTTPS in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\URL;
 use App\Models\User;
 
 class AppServiceProvider extends ServiceProvider
@@ -15,6 +16,11 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
+        // Paksa HTTPS saat di production
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
+
         // Mengirimkan data $users ke seluruh file blade otomatis
         View::composer('*', function ($view) {
             $view->with('users', User::all());
